8efa2c214a282f57ffd9816a985f801665fc8597
Pavel <frost1449@gmail.com>
2022-06-06 06:02:41 +0300

    Added pageObjects

--- src/test/java/tests/DemoqaTests.java
+++ src/test/java/tests/DemoqaTests.java
@@ -1,15 +1,6 @@
 package tests;
 
-import com.codeborne.selenide.Selectors;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.Test;
-
-import java.nio.channels.Selector;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selenide.$;
 
 public class DemoqaTests extends TestBase {
 
@@ -22,52 +13,31 @@ public class DemoqaTests extends TestBase {
         String subjects = "Maths";
         String address = "Moscow, Petrovka, 38";
 
-        open("/automation-practice-form");
-        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
-        executeJavaScript("$('footer').remove()");
-        executeJavaScript("$('#close-fixedban').remove()");
+        registrationFormPage.openPage()
+                .setFirstName(name)
+                .setLastName(surname)
+                .setUserEmail(email)
+                .setGenter("Male")
+                .setUserNumber(mobile)
+                .setDateOfBirth("01","June","2000")
+                .setSubjects(subjects)
+                .setHobbies("Music")
+                .setUploadPicture("imeg.jpg")
+                .setCurrentAddress(address)
+                .setState("Rajasthan")
+                .setCity("Jaipur")
+                .setSubmit()
+                .openTable();
 
-        $("#firstName").setValue(name);
-        $("#lastName").setValue(surname);
-        $("#userEmail").setValue(email);
-        $("#genterWrapper").$(Selectors.byText("Male")).click();
-        $("#userNumber").setValue(mobile);
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption("June");
-        $(".react-datepicker__year-select").selectOption("2000");
-        $(".react-datepicker__day--001:not(.react-datepicker__day--outside-month)").click();
-        $("#subjectsInput").sendKeys(subjects);
-        $("#subjectsInput").pressEnter();
-        $("#hobbiesWrapper").$(Selectors.byText("Music")).click();
-        $("input#uploadPicture").uploadFromClasspath("imeg.jpg");
-        $("#currentAddress").setValue(address);
-        $("#state").click();
-        $("#stateCity-wrapper").$(Selectors.byText("Rajasthan")).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(Selectors.byText("Jaipur")).click();
-        $("#submit").pressEnter();
-
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-
-        checkTable("Student Name", name + " " + surname, "Student Email", email, "Gender",
-                "Male", "Mobile", mobile, "Date of Birth", "01 June,2000", "Subjects",
-                subjects, "Hobbies", "Music", "Picture", "imeg.jpg", "Address", address,
-                "State and City", "Rajasthan Jaipur");
-
-    }
-
-    void checkTable(String key1, String value1, String key2, String value2, String key3, String value3, String key4,
-                    String value4, String key5, String value5, String key6, String value6, String key7, String value7,
-                    String key8, String value8, String key9, String value9, String key10, String value10) {
-        $(".table-responsive").$(Selectors.byText(key1)).parent().shouldHave(text(value1));
-        $(".table-responsive").$(Selectors.byText(key2)).parent().shouldHave(text(value2));
-        $(".table-responsive").$(Selectors.byText(key3)).parent().shouldHave(text(value3));
-        $(".table-responsive").$(Selectors.byText(key4)).parent().shouldHave(text(value4));
-        $(".table-responsive").$(Selectors.byText(key5)).parent().shouldHave(text(value5));
-        $(".table-responsive").$(Selectors.byText(key6)).parent().shouldHave(text(value6));
-        $(".table-responsive").$(Selectors.byText(key7)).parent().shouldHave(text(value7));
-        $(".table-responsive").$(Selectors.byText(key8)).parent().shouldHave(text(value8));
-        $(".table-responsive").$(Selectors.byText(key9)).parent().shouldHave(text(value9));
-        $(".table-responsive").$(Selectors.byText(key10)).parent().shouldHave(text(value10));
+        registrationFormPage.checkResult("Student Name", name + " " + surname)
+                        .checkResult("Student Email", email)
+                        .checkResult("Gender","Male")
+                        .checkResult("Mobile", mobile)
+                        .checkResult("Date of Birth", "01 June,2000")
+                        .checkResult("Subjects", subjects)
+                        .checkResult("Hobbies", "Music")
+                        .checkResult("Picture", "imeg.jpg")
+                        .checkResult("Address", address)
+                        .checkResult("State and City", "Rajasthan Jaipur");
     }
 }
